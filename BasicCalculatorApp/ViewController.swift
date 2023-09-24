3a0988cd401e4b215d73d157393b427ff5a2d3ba
Burak Yildiz <burakkyildiz@icloud.com>
2023-09-24 16:03:05 +0300

    added operation func

--- BasicCalculatorApp/ViewController.swift
+++ BasicCalculatorApp/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0
+    var resultF : Float = 0 // float sonuc degeri
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addClicked(_ sender: Any) {
+        if let firstValue = Int(firstText.text!){
+            if let secondValue = Int(secondText.text!){
+                
+                let result :Int  = firstValue + secondValue
+                resultLabel.text = String(result)
+            }
+        }
+            
+    }
+    
+    @IBAction func subtractClicked(_ sender: Any) {
+        if let firstValue = Int(firstText.text!){
+            if let secondValue = Int(secondText.text!){
+                
+                result = firstValue - secondValue
+                resultLabel.text = String(result)
+            }
+        }
+        
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstValue = Int(firstText.text!){
+            if let secondValue = Int(secondText.text!){
+                
+                result = firstValue * secondValue
+                resultLabel.text = String(result)
+            }
+        }
+        
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstValue = Float(firstText.text!){
+            if let secondValue = Float(secondText.text!){
+                
+                resultF = firstValue / secondValue
+                resultLabel.text = String(resultF)
+            }
+        }
+        
+    }
+    
 }
 
